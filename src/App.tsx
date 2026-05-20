6866d3e1f9f9248ecde5761a47875625b91cbe31
Victor <victorm.martinezjr@outlook.com>
2026-05-19 21:09:24 -0400

    feat(ui): Create Message section with scrollTrigger animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import { ScrollTrigger } from "gsap/all";
+import Message from "./sections/Message";
+import Flavor from "./sections/Flavor";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Message />
+      <Flavor />
     </main>
   );
 };
